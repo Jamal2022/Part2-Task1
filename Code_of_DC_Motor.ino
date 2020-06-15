4fc3298fd0850b542b211936b420e525b08d9965
Jamal Ali <65658715+Jamal2022@users.noreply.github.com>
2020-06-15 13:37:06 +0100

    Update Code_of_DC_Motor.ino

--- Code_of_DC_Motor.ino
+++ Code_of_DC_Motor.ino
@@ -1,20 +1,20 @@
 
 //Motor L298N
-const byte avantM1=8;//Right
-const byte arriereM1=9;
-const byte vitesseM1=10;
-const byte avantM2=6;//Lift
-const byte arrierM2=7;
-const byte vitesseM2=5;
+const byte BackM1=8;//Right
+const byte ForM1=9;
+const byte SpeedM1=10;
+const byte BackM2=6;//Lift
+const byte ForM2=7;
+const byte SpeedM2=5;
 int Speed = 150;
 void setup (){
   //Moteur L298N 
-  pinMode(vitesseM1,OUTPUT);
-  pinMode(vitesseM2,OUTPUT);
-  pinMode(avantM1,OUTPUT);
-  pinMode(arriereM1,OUTPUT);
-  pinMode(avantM2,OUTPUT);
-  pinMode(arrierM2,OUTPUT);
+  pinMode(SpeedM1,OUTPUT);
+  pinMode(SpeedM2,OUTPUT);
+  pinMode(BackM1,OUTPUT);
+  pinMode(ForM1,OUTPUT);
+  pinMode(BackM2,OUTPUT);
+  pinMode(ForM2,OUTPUT);
   
   Serial.begin(9600);
 }
@@ -22,51 +22,51 @@ void setup (){
 
 void loop(){
 //Forward 
-analogWrite(vitesseM1,Speed);//Speed
-digitalWrite(arriereM1,LOW);  
-digitalWrite(avantM1,HIGH);
+analogWrite(SpeedM1,Speed);//Speed
+digitalWrite(ForM1,LOW);  
+digitalWrite(BackM1,HIGH);
 
-analogWrite(vitesseM2,Speed);
-digitalWrite(arrierM2,LOW);  
-digitalWrite(avantM2,HIGH);
+analogWrite(SpeedM2,Speed);
+digitalWrite(ForM2,LOW);  
+digitalWrite(BackM2,HIGH);
 delay(2000);
 
 //Right
-analogWrite(vitesseM1,Speed);
-digitalWrite(arriereM1,HIGH);  
-digitalWrite(avantM1,LOW);
+analogWrite(SpeedM1,Speed);
+digitalWrite(ForM1,HIGH);  
+digitalWrite(BackM1,LOW);
 
-analogWrite(vitesseM2,Speed);
-digitalWrite(arrierM2,LOW);  
-digitalWrite(avantM2,HIGH);
+analogWrite(SpeedM2,Speed);
+digitalWrite(ForM2,LOW);  
+digitalWrite(BackM2,HIGH);
 delay(2000);
 
 //Lift 
 
-analogWrite(vitesseM1,Speed);
-digitalWrite(arriereM1,LOW);  
-digitalWrite(avantM1,HIGH);
+analogWrite(SpeedM1,Speed);
+digitalWrite(ForM1,LOW);  
+digitalWrite(BackM1,HIGH);
 
-analogWrite(vitesseM2,Speed);
-digitalWrite(arrierM2,HIGH);  
-digitalWrite(avantM2,LOW);
+analogWrite(SpeedM2,Speed);
+digitalWrite(ForM2,HIGH);  
+digitalWrite(BackM2,LOW);
 delay(2000);
      
 //Backward
 
-analogWrite(vitesseM1,Speed);
-digitalWrite(arriereM1,HIGH);  
-digitalWrite(avantM1,LOW);
+analogWrite(SpeedM1,Speed);
+digitalWrite(ForM1,HIGH);  
+digitalWrite(BackM1,LOW);
 
-analogWrite(vitesseM2,Speed);
-digitalWrite(arrierM2,HIGH);  
-digitalWrite(avantM2,LOW);
+analogWrite(SpeedM2,Speed);
+digitalWrite(ForM2,HIGH);  
+digitalWrite(BackM2,LOW);
 delay(2000);
 
 //Stop
-digitalWrite(arriereM1,LOW);
-digitalWrite(avantM1,LOW);
-digitalWrite(arrierM2,LOW);  
-digitalWrite(avantM2,LOW); 
+digitalWrite(ForM1,LOW);
+digitalWrite(BackM1,LOW);
+digitalWrite(ForM2,LOW);  
+digitalWrite(BackM2,LOW); 
 delay(1000);  
 }
